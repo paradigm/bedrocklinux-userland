68f3828d9fb60ffbba933a0be43fb8c6db5f1939
Yuxuan Shui <yshuiv7@gmail.com>
2016-06-19 20:22:30 -0400

    Verify file is at least accessible by root before SET_CALLER_UID
    Reduce the number of seteuid() calls, which is quite heavy

--- src/brp/brp.c
+++ src/brp/brp.c
@@ -611,7 +611,13 @@ int corresponding(char *in_path, int *out_fd, struct stat *stbuf,
 	char tmp_path[PATH_MAX + 1];
 	int *stratum_root_fd = malloc(sizeof(int) * nstratum);
 
-	int ret = chdir(STRATA_ROOT);
+	int ret = seteuid(0);
+	if (unlikely(ret < 0)) {
+		perror("seteuid");
+		exit(1);
+	}
+
+	ret = chdir(STRATA_ROOT);
 	if (unlikely(ret < 0)) {
 		// strata root missing?!
 		perror("Failed to chdir to strata root");
@@ -625,18 +631,12 @@ int corresponding(char *in_path, int *out_fd, struct stat *stbuf,
 		if (unlikely(stratum_root_fd[st] < 0))
 			continue;
 
-		ret = seteuid(0);
-		if (unlikely(ret < 0))
-			return -errno;
-
 		ret = fchdir(stratum_root_fd[st]);
 		if (unlikely(ret < 0))
 			continue;
 		ret = chroot(".");
 		if (unlikely(ret < 0))
 			continue;
-
-		SET_CALLER_UID();
 
 		/* check for a match on something contained in one of the configured
 		 * directories */
@@ -659,6 +659,19 @@ int corresponding(char *in_path, int *out_fd, struct stat *stbuf,
 				ret = stat(tmp_path, stbuf);
 				if (ret < 0)
 					continue;
+
+				// Check again with proper permission
+				SET_CALLER_UID();
+				ret = stat(tmp_path, stbuf);
+				if (unlikely(ret < 0)) {
+					ret = seteuid(0);
+					if (unlikely(ret < 0)) {
+						perror("seteuid");
+						exit(1);
+					}
+					continue;
+				}
+
 				*arg_out_item = &out_items[i];
 				*arg_in_item = &out_items[i].in_items[j];
 				*stratum_id = st;
@@ -689,6 +702,18 @@ int corresponding(char *in_path, int *out_fd, struct stat *stbuf,
 				int ret = stat(in_item[j].path, stbuf);
 				if (ret < 0)
 					continue;
+
+				// Check again with proper permission
+				SET_CALLER_UID();
+				ret = stat(in_item[j].path, stbuf);
+				if (unlikely(ret < 0)) {
+					ret = seteuid(0);
+					if (unlikely(ret < 0)) {
+						perror("seteuid");
+						exit(1);
+					}
+					continue;
+				}
 				*arg_out_item = &out_items[i];
 				*arg_in_item = &out_items[i].in_items[j];
 				*stratum_id = st;
